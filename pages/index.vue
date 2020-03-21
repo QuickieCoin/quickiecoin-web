db4367734afc1a97622a6ef901d804e59419df53
Sam Feder <sam.feder1@gmail.com>
2020-03-21 11:18:12 -0400

    add smallchat

--- pages/index.vue
+++ pages/index.vue
@@ -56,7 +56,7 @@ export default {
   },
   head () {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      script: [{ src: 'https://embed.small.chat/TBRGBNPL0GCCGPR9KK.js' }]
     }
   }
 }
