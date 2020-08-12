2af263b5e9aa903a901d87c1df0f61f6b1f14026
Sam Feder <sam.feder1@gmail.com>
2020-08-12 16:05:10 -0400

    just fkn get rid of stupid eslint module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,72 +1,86 @@
-require('dotenv').config()
+require("dotenv").config();
 
 export default {
-  mode: 'universal',
+  mode: "universal",
   head: {
-    title: 'QuickieCoin',
-    meta: [{
-        charset: 'utf-8'
+    title: "QuickieCoin",
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ]
   },
   loading: {
-    color: '#fff'
+    color: "#fff"
   },
   css: [],
-  plugins: [{
-      src: '~/plugins/mapbox',
-      mode: 'client'
+  plugins: [
+    {
+      src: "~/plugins/mapbox",
+      mode: "client"
     },
     {
-      src: '~/plugins/ga',
-      mode: 'client'
+      src: "~/plugins/ga",
+      mode: "client"
     },
     {
-      src: '~/plugins/hotjar',
-      mode: 'client'
+      src: "~/plugins/hotjar",
+      mode: "client"
     }
   ],
   buildModules: [
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/dotenv'
+    // '@nuxtjs/eslint-module',
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/dotenv"
   ],
   modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/markdownit',
-    ['nuxt-sanity', {
-      projectId: 'dkkl7znc',
-      dataset: 'production'
-    }],
-    ['nuxt-gmaps', {
-      key: process.env.GOOGLE_MAPS_API_KEY
-      // you can use libraries: ['places']
-    }],
-    ['nuxt-fontawesome', {
-      imports: [{
-          set: '@fortawesome/free-solid-svg-icons',
-          icons: ['fas']
-        },
-        {
-          set: '@fortawesome/free-brands-svg-icons',
-          icons: ['fab']
-        }
-      ]
-    }]
+    "@nuxtjs/axios",
+    "@nuxtjs/markdownit",
+    [
+      "nuxt-sanity",
+      {
+        projectId: "dkkl7znc",
+        dataset: "production"
+      }
+    ],
+    [
+      "nuxt-gmaps",
+      {
+        key: process.env.GOOGLE_MAPS_API_KEY
+        // you can use libraries: ['places']
+      }
+    ],
+    [
+      "nuxt-fontawesome",
+      {
+        imports: [
+          {
+            set: "@fortawesome/free-solid-svg-icons",
+            icons: ["fas"]
+          },
+          {
+            set: "@fortawesome/free-brands-svg-icons",
+            icons: ["fab"]
+          }
+        ]
+      }
+    ]
   ],
   axios: {},
   markdownit: {
@@ -81,4 +95,4 @@ export default {
       devtools: true
     }
   }
-}
+};
