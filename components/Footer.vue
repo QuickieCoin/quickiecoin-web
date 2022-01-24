650d089ccf1b86f62f6b8710249d3686e98ced1f
Sam Feder <sam.feder1@gmail.com>
2022-01-24 15:20:15 -0500

    clean up footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,23 +1,25 @@
 <template>
-  <footer class="flex justify-between items-center border-t-2 py-5 mb-16">
-    <div>
+  <footer class="flex-column justify-between items-center border-t-2 mb-8">
+    <div class=mt-3>
       <ul class="flex row">
-        <li class="px-5">
+        <li class="px-5 m-auto">
           <a class="nav-links text-blue-900" target="_blank" href="/privacy">Privacy Policy</a>
         </li>
-        <li class="px-5">
+        <li class="px-5 m-auto">
           <a class="nav-links text-blue-900" target="_blank" href="/terms">Terms and Conditions</a>
         </li>
       </ul>
     </div>
-    <div class="flex flex-row">
-      <a v-for="social in socials" :href="`http://${social}.com/quickiecoin`" :key="`social-${social}`" target="_blank">
-        <img :height="iconSize" :width="iconSize" :src="logos[social]" class="mx-2">
-      </a>
-      <a class="flex flex-row" href="mailto:team@quickiecoin.com">
-        <img :height="iconSize" :width="iconSize" :src="logos.mail" class="mx-2">
-        <span class="mr-2">team@quickiecoin.com</span>
-      </a>
+    <div class="flex flex-row mt-5">
+      <div class="m-auto">
+        <a v-for="social in socials" :href="`http://${social}.com/quickiecoin`" :key="`social-${social}`" target="_blank">
+          <img :height="iconSize" :width="iconSize" :src="logos[social]" class="inline mx-1">
+        </a>
+        <a href="mailto:team@quickiecoin.com">
+          <img :height="iconSize" :width="iconSize" :src="logos.mail" class="inline mx-1">
+          <span>team@quickiecoin.com</span>
+        </a>
+      </div>
     </div>
   </footer>
 </template>
